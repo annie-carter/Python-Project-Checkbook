3ac659488275e1580ecf8a3ce907ec30b446aad5
annie-carter <annie.carter831@gmail.com>
2023-05-15 23:33:50 -0500

    added number opts, although not specified in rubric :-)

--- checkbook.py
+++ checkbook.py
@@ -3,8 +3,14 @@ def keep_going():
     keep_going = input('Would you like to make another transaction? select y or n: ')
     if keep_going == 'y':
         main()
-    if keep_going == 'n':
+    elif keep_going == 'n':
         exit_option()
+    else:
+        print('That is an invalid input. Please try again')
+        main()
+    
+
+
 
 import json
 
@@ -22,22 +28,33 @@ def balance_option(acct_balance):
 # The crediting option of the application operates appropriately
 def credit_option(acct_balance):
     credit_input = input('How much would you like to credit this account: $')
-    acct_balance = float(acct_balance) + float(credit_input)
-    print(f'Your new balance after credit is: $ {acct_balance}')
-    close_ledger(acct_balance)
-    keep_going()
-    return acct_balance
+    if credit_input.isdigit() == False: 
+        print('That is an invalid input. Please try again')
+        main()
+    else:
+        acct_balance = float(acct_balance) + float(credit_input)
+        print(f'Your new balance after credit is: $ {acct_balance}')
+        close_ledger(acct_balance)
+        keep_going()
+        return acct_balance
 
 # The debit option of the application operates appropriately
 def debit_option(acct_balance):
-    debit_input = input ('How much would you like to withdraw: $')
-    acct_balance = float(acct_balance) - float(debit_input)
-    print(f'Your new balance after debit is: $ {acct_balance}')
-    close_ledger(acct_balance)
-    keep_going()
-    return acct_balance
+    debit_input = input('How much would you like to withdraw: $')
+    if debit_input.isdigit() == False: 
+        print('That is an invalid input. Please try again')
+        main()
+    else:
+        debit_amount = float(debit_input)
+        new_balance = float(acct_balance) - debit_amount
+        if debit_input.isdigit():
+            print(f'Your new balance after debit is: ${new_balance}')
+            close_ledger(new_balance)
+            keep_going()
+            return acct_balance
 
 #The checkbook application can be closed appropriately
+
 def exit_option():
     exit_input = input('Are you sure you want to exit? select y or n:')
     if exit_input == 'y':
@@ -46,7 +63,9 @@ def exit_option():
         print('You will be returned to the main screen')
         main()
     else:
-        print('That is an invalid input. Please select y or n:')
+        print('That is an invalid input.')
+        main()
+
 
 #View Balance, Credit, Debit, and Exit
 # #1. Basic functionality
@@ -56,32 +75,35 @@ def main():
     ledger = open('ledger.txt')
     acct_balance = ledger.read()
     
-    print('\n\nWelcome to the Bank of Carter! \nPlease type option from the menu as prompted \nor you will be removed from the bank? \n\n')
-    print('balance\ncredit\ndebit\nexit\n')
+    print('\n\nWelcome to the Bank of Carter! \nPlease review Terms of Use Agreement in README.md \nPlease type option from the menu as prompted \nor you will be removed from the bank? \n\n')
+    print('1 = balance\n2 = credit\n3 = debit\n4 = exit\n')
 
 
 
-    balance = 'Balance'
-    credit = 'Credit'
-    debit = 'Debit '
-    exit_checkbook = 'Exit'
+#     balance = 'Balance'
+#     credit = 'Credit'
+#     debit = 'Debit '
+#     exit = 'Exit'
 
     choice = input()
 
-    if choice == 'balance':
+    if choice == '1':
         print( 'You selected balance')
         balance_option(acct_balance)
-    elif choice == 'credit':
-        print('You selected credit')
+    elif choice == '2':
+        print('You selected credit account')
         credit_option(acct_balance)
-    elif choice == 'debit':
-        print('You selected debit')
+    elif choice == '3':
+        print('You selected debit account')
         debit_option(acct_balance)
-    elif choice == 'exit':
+    elif choice == '4':
         print('You selected exit checkbook')
         exit_option()
-    else:
-        print('That is an invalid input. Please select y or n:')
+    elif not choice.isdigit():
+        print('That is an invalid input. Please try again')
+        main()
     # close_ledger()
+
+
 
 main()
